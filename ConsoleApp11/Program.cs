1ffdb607e69e2f621a953d3aad4f8b41942a264f
User <User@WIN-ODUS31S7641>
2023-04-09 20:33:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rubles = 100;
+            var dollars = 10;
+            var euro = 3;
+
+            var courseDollars = 80.5;
+            var courseEuro = 90.32;
+            int whatConvert, count, inConvert;
+
+            Console.WriteLine("Что вы хотите конвертировать? \n1-рубли \n2-доллары \n3-евро");
+            whatConvert = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сколько?");
+            count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Во что вы конвертировать? \n1-рубли \n2-доллары \n3-евро");
+            inConvert = Convert.ToInt32(Console.ReadLine());
+        }
+        static int Converter (int whatConvert, int inConvert)
+        {
+            return 
+        }
+    }
+}
+
